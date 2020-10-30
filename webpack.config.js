0b9e19cf02334ae6ad1e52ab457c614e225f4a32
aplikadote <aplikadito@gmail.com>
2020-10-30 18:16:21 -0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
+var path = require('path');
+
 module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.js'
+    },    
     module: {
         rules: [
             {
@@ -11,7 +18,8 @@ module.exports = {
                     // Compiles Sass to CSS
                     'sass-loader',
                 ],
-            },
+            }
+        
         ],
-    },
+    }
 };
