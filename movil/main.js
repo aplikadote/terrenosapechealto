762a7fb310430fc08e4521cfdb4900f3ea885de5
aplikadote <aplikadito@gmail.com>
2020-12-07 16:48:00 -0300

    version movil

---
+++ movil/main.js
@@ -0,0 +1,20 @@
+import './style.scss';
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+import { Loader } from "@googlemaps/js-api-loader"
+
+// loads the Icon plugin
+UIkit.use(Icons);
+
+const key = "AIzaSyALGe5_JX1x8Po0MsfJ9fVk-oytY2e7x00";
+
+const loader = new Loader({
+    apiKey: key,
+    version: "weekly"    
+});
+loader.load().then(() => {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -42.8352686, lng: -73.687716 },
+        zoom: 12.21,        
+    });
+});
